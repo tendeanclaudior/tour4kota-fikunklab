3387250feab333e95dc0ff0d4a9cb408db3002b0
andreydesayli <andreydesylibororing@gmail.com>
2022-08-19 14:21:30 +0800

    menambahkan last page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import { AuthContextProvider } from "./context/AuthContext";
 import Registration from "./pages/Registration";
+import LastPage from "./pages/LastPage";
 
 function App() {
   return (
     <>
       <AuthContextProvider>
         <Routes>
-          <Route path="/" element={<SignIn />} />
+          {/* <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/regis" element={<Registration />} />
+          <Route path="/regis" element={<Registration />} /> */}
+          <Route path="/lastpage" element={<LastPage />} />
         </Routes>
       </AuthContextProvider>
     </>
---
+++ src/pages/LastPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import JarankPulang from '../assets/logo/jarankPulang.png'
+import Footer from '../components/Footer'
+
+const LastPage = ({nama="Budi Doremi"}) => {
+  return (
+    <div className=' flex-col'>
+        <div className='flex justify-center mb-[100px]'>
+            <div className='bg-gradient-to-b from-[#1B8D42] via-[#FF0000] to-[#FFDD00] p-[1px] mt-[129px] rounded-lg shadow-2xl'>
+            <div className='flex w-[244px] h-[382px] bg-white border items-center flex-col rounded-lg relative'>
+                <h1 className='text-[14px] text-[#A61E22] mt-[20px] font-poppins font-semibold'>Thank You</h1>
+                <h1 className='text-[11px] text-[#9E9E9E] mt-[8px] font-poppins font-semibold'>{nama}</h1>
+                <img src={JarankPulang} alt="" className="w-[157px] h-[143px] mt-[45px] animate-pulse" />
+                <h1 className='font-poppins font-medium text-[10px] text-center mt-[67px]'>Terima Kasih sudah menjadi bagian dari Tour & Concert JarankPulang</h1>
+            </div>
+            </div>
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default LastPage
